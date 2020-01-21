eef364354952b7bc6ca78551547842b391232c6d
david biton <52914672+davidbiton33@users.noreply.github.com>
2020-01-21 12:36:42 +0200

    Add files via upload

---
+++ src/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import List from './list';
+
+
+function Home() {
+  return (
+    <div>
+   <h1> רשימת קניות</h1>
+    <List />
+    </div>
+  );
+}
+
+export default Home;
